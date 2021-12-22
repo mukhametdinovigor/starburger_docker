901890ab9c44761565986b09dfffec78267c0f45
mukhametdinovigor <mukhametdinovigor@gmail.com>
2021-12-22 23:41:27 +1100

    Set up prod environment

--- prod_env.sh
+++ prod_env.sh
@@ -2,13 +2,12 @@
 
 git pull
 
-docker-compose down
-cp ./.env.prod ./star-burger/.env
-docker-compose up -d
-docker-compose exec web python star-burger/manage.py migrate
-cp -r /var/lib/docker/volumes/starburger_docker_bundles_volume/_data/. /var/lib/docker/volumes/starburger_docker_static_volume/_data
-rm -f /var/lib/docker/volumes/starburger_docker_conf/_data/default.conf
-cp ./nginx.conf /var/lib/docker/volumes/starburger_docker_conf/_data
-docker-compose restart nginx
+docker-compose -f docker-compose.prod.yml down
+docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f docker-compose.prod.yml exec web python star-burger/manage.py migrate
+sudo cp -r /var/lib/docker/volumes/starburger_docker_bundles_volume/_data/. /var/lib/docker/volumes/starburger_docker_static_volume/_data
+sudo rm -f /var/lib/docker/volumes/starburger_docker_conf/_data/default.conf
+sudo cp ./nginx.conf /var/lib/docker/volumes/starburger_docker_conf/_data
+docker-compose -f docker-compose.prod.yml restart nginx
 
 echo "Deploy is done"
